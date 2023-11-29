ab4d19683d6d6bdc5536960fb67fdb5f1620f481
Dane Hoffman <dhoffman@Danes-MacBook-Air.local>
2023-11-29 14:56:52 -0700

    Created Pet Finder API

--- app.js
+++ app.js
@@ -2,12 +2,54 @@ const express = require('express');
 const app = express();
 const PORT = 8080;
 
-app.listen(PORT, () => { console.log(`Server is running on port ${PORT}`); });    
-app.get('/hello', (req, res) => {
-    res.send('Hello World!'); });
+// Simulated database of pets
+const petsDB = [
+    { id: 1, name: 'Fluffy', owner: 'Alice' },
+    { id: 2, name: 'Buddy', owner: 'Bob' },
+    // Add more pets here
+];
 
-app.get('/hello/:name', (req, res) => {
-    const name = req.params.name; res.send(`Hello ${name}`); });
+// Endpoint to get all pets
+app.get('/api/v1/pets', (req, res) => {
+    // Respond with the entire pets database as JSON
+    res.json(petsDB);
+});
 
-app.get('/hello-query', (req, res) => {
-    const name = req.query.name; res.send(`Hello ${name}`); });   
+// Endpoint to get a pet by name
+app.get('/api/v1/pets/:name', (req, res) => {
+    // Extract the pet name from the URL parameter
+    const petName = req.params.name;
+    
+    // Search for the pet in the database based on the name
+    const pet = petsDB.find(pet => pet.name === petName);
+    
+    // If pet not found, respond with a 404 error message
+    if (!pet) {
+        res.status(404).send('Pet not found');
+    } else {
+        // If pet found, respond with the pet details as JSON
+        res.json(pet);
+    }
+});
+
+// Endpoint to get a pet by owner's name using a query string
+app.get('/api/v1/pets/owner', (req, res) => {
+    // Extract the owner's name from the query parameter (?owner=name)
+    const ownerName = req.query.owner;
+    
+    // Search for the pet in the database based on the owner's name
+    const pet = petsDB.find(pet => pet.owner === ownerName);
+    
+    // If pet not found for the owner, respond with a 404 error message
+    if (!pet) {
+        res.status(404).send('Pet not found for this owner');
+    } else {
+        // If pet found, respond with the pet details as JSON
+        res.json(pet);
+    }
+});
+
+// Start the server and listen on the specified port
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
